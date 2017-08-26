768ad5cd5d2027beb40fe83e59b1c190f20d4994
keri west <snailgenie@gmail.com>
2017-08-26 18:53:57 +0000

    automatically backed up by learn

--- tests.js
+++ tests.js
@@ -5,6 +5,7 @@ if (index.name === "Susan") {
 } else {
     console.log("Expected: Susan, Received: "+index.name)
 }
+
 if (index.height === "70") {
     console.log("The height is correct")
 } else {

6af22697d179c6054c7cb49f999874521f316ff0
keri west <snailgenie@gmail.com>
2017-08-26 19:14:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,15 +1,6 @@
 var name = "Susan"
 var height = "70"
-if (name === "Susan") {
-    console.log("Name is correct")
-} else {
-  console.log("Expected Susan")
-}
-if (height === "70") {
-  console.log("Height is correct")
-} else {
-  console.log("Expected 70")
-}
+
 // Don't worry about this
 module.exports = { name, height
 }
